befea68239467e2e9f659e4472b67051db3f6b37
CristianGonzalez27 <crisfegg8@gmail.com>
2025-04-24 23:05:50 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const multer = require("multer");
+const ffmpeg = require("fluent-ffmpeg");
+const ffmpegPath = require("ffmpeg-static");
+const fs = require("fs");
+const cors = require("cors");
+
+const app = express();
+const upload = multer({ dest: "uploads/" });
+ffmpeg.setFfmpegPath(ffmpegPath);
+
+app.use(cors());
+
+app.post("/convert", upload.single("video"), (req, res) => {
+  const inputPath = req.file.path;
+  const outputPath = `outputs/${req.file.filename}.mp3`;
+
+  ffmpeg(inputPath)
+    .noVideo()
+    .audioCodec("libmp3lame")
+    .save(outputPath)
+    .on("end", () => {
+      res.download(outputPath, () => {
+        fs.unlinkSync(inputPath);
+        fs.unlinkSync(outputPath);
+      });
+    })
+    .on("error", (err) => {
+      console.error(err);
+      res.status(500).send("Error al convertir el archivo.");
+    });
+});
+
+app.listen(3000, () => {
+  console.log("Servidor listo en puerto 3000");
+});
